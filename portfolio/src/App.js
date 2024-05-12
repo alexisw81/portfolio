82b96e68533a3deb8406f5f467e27a1a24aa18a1
alexisw81 <59579888+alexisw81@users.noreply.github.com>
2024-05-11 21:22:24 -0400

    setState Function

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
 
-class RegularClass { }
-class ComponentClass extends Component { }
-
-const regularClassInstance = new RegularClass();
-const componentClassInstance = new ComponentClass();
-
-console.log('regularClassInstance', regularClassInstance);
-console.log('componentClassInstance', componentClassInstance);
-
 class App extends Component {
+
+    state = { displayBio: false };
+
+    togggleDisplayBio = () => {
+        this.setState({ displayBio: !this.state.displayBio });
+    }
+
     render() {
         return (
             <div>
                 <h1>Hello!</h1>
                 <p>My name is Alexis. I am a software engineer.</p>
                 <p>I'm always looking forward to working on meaningful projects.</p>
+                {
+                    this.state.displayBio ? (
+                        <div>
+                            <p>I live in Maryland, and I code every day.</p>
+                            <p>My favorite language is Javascript, and I think React.js is awesome.</p>
+                            <p>Besides coding, I love rock climbing and cats!</p>
+
+                            <button onClick={this.togggleDisplayBio}>Show Less</button>
+                        </div>
+
+
+                    ) : (
+                        <div>
+                            <button onClick={this.togggleDisplayBio}>Read More</button>
+                        </div>
+                    )
+                }
             </div>
         );
     }
