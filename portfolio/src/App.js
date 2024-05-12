2fa3872d36dcec89b2fdb4fe09ea8a56450280c7
alexisw81 <59579888+alexisw81@users.noreply.github.com>
2024-05-11 20:18:21 -0400

    Classes and Instances

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,8 +1,24 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from 'react';
 
-function App() {
-  return <div className="App">Hello React</div>;
+class RegularClass { }
+class ComponentClass extends Component { }
+
+const regularClassInstance = new RegularClass();
+const componentClassInstance = new ComponentClass();
+
+console.log('regularClassInstance', regularClassInstance);
+console.log('componentClassInstance', componentClassInstance);
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello!</h1>
+                <p>My name is Alexis. I am a software engineer.</p>
+                <p>I'm always looking forward to working on meaningful projects.</p>
+            </div>
+        );
+    }
 }
 
 export default App;
