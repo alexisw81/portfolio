4da3ac630daf9a97c64534e3fe7b23e4596cb6c6
alexisw81 <59579888+alexisw81@users.noreply.github.com>
2024-05-25 20:49:57 -0400

    Adding Project Components

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,39 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Projects from "./Projects";
 
 class App extends Component {
+  state = { displayBio: false };
 
-    state = { displayBio: false };
+  togggleDisplayBio = () => {
+    this.setState({ displayBio: !this.state.displayBio });
+  };
 
-    togggleDisplayBio = () => {
-        this.setState({ displayBio: !this.state.displayBio });
-    }
+  render() {
+    return (
+      <div>
+        <h1>Hello!</h1>
+        <p>My name is Alexis. I am a software engineer.</p>
+        <p>I'm always looking forward to working on meaningful projects.</p>
+        {this.state.displayBio ? (
+          <div>
+            <p>I live in Maryland, and I code every day.</p>
+            <p>
+              My favorite language is Javascript, and I think React.js is
+              awesome.
+            </p>
+            <p>Besides coding, I love rock climbing and cats!</p>
 
-    render() {
-        return (
-            <div>
-                <h1>Hello!</h1>
-                <p>My name is Alexis. I am a software engineer.</p>
-                <p>I'm always looking forward to working on meaningful projects.</p>
-                {
-                    this.state.displayBio ? (
-                        <div>
-                            <p>I live in Maryland, and I code every day.</p>
-                            <p>My favorite language is Javascript, and I think React.js is awesome.</p>
-                            <p>Besides coding, I love rock climbing and cats!</p>
-
-                            <button onClick={this.togggleDisplayBio}>Show Less</button>
-                        </div>
-
-
-                    ) : (
-                        <div>
-                            <button onClick={this.togggleDisplayBio}>Read More</button>
-                        </div>
-                    )
-                }
-            </div>
-        );
-    }
+            <button onClick={this.togggleDisplayBio}>Show Less</button>
+          </div>
+        ) : (
+          <div>
+            <button onClick={this.togggleDisplayBio}>Read More</button>
+          </div>
+        )}
+        <hr />
+        <Projects />
+      </div>
+    );
+  }
 }
 
 export default App;
